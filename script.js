c619a0d0ea1a28359d861dbc1130fd0925bac580
Vlad-Marko <druzhytskyivlad@gmail.com>
2025-06-03 19:30:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,399 @@
+const countryData = {
+  "Grand final": {
+    flag: "/img/channels4_profile_40px.jpg",
+
+    video: "https://www.youtube.com/embed/xPSn888JAFU?si=SOWlxS2EbgUBGkui",
+    singer: "",
+    song: "",
+    lyrics: "Переклад пісні тут...",
+  },
+  "First Semi-Final": {
+    flag: "/img/channels4_profile_40px.jpg",
+
+    video: "https://www.youtube.com/embed/0HNXVB2UQwU?si=8jHoZMsY3vYYFrp9",
+
+    singer: "",
+    song: "",
+    lyrics: "Переклад пісні тут...",
+  },
+  "Second Semi-Final": {
+    flag: "/img/channels4_profile_40px.jpg",
+    video: "https://www.youtube.com/embed/AM9TxEJMyco?si=YjATy9JCDInTIc7B",
+    singer: "",
+    song: "",
+    lyrics: "Переклад пісні тут...",
+  },
+  Ісландія: {
+    flag: "https://flagcdn.com/w40/is.png",
+    video: "https://www.youtube.com/embed/c73Lx1QUZZA?si=yGaYDBxFdMwu62Bq",
+    singer: "VÆB",
+    song: "RÓA",
+    lyrics: "Переклад пісні тут...",
+  },
+  Польща: {
+    flag: "https://flagcdn.com/w40/pl.png",
+
+    video: "https://www.youtube.com/embed/NQXhvclWuPE?si=HXm2ehvO5nWDydeN",
+
+    singer: "Justyna Steczkowska",
+    song: "GAJA",
+    lyrics: "Переклад пісні тут...",
+  },
+  Словенія: {
+    flag: "https://flagcdn.com/w40/si.png",
+    video: "https://www.youtube.com/embed/Jbs9WlvIkg0?si=rXrwOzOqSWcxpL-K",
+    singer: "Klemen",
+    song: "How Much Time Do We Have Left",
+    lyrics: "Переклад пісні тут...",
+  },
+  Естонія: {
+    flag: "https://flagcdn.com/w40/ee.png",
+
+    video: "https://www.youtube.com/embed/F3wsy8bywXQ?si=UPXxRBdkP--s7XUC",
+
+    singer: "Tommy Cash",
+    song: "Espresso Macchiato",
+
+    lyrics: "Переклад пісні тут...",
+  },
+  Іспанія: {
+    flag: "https://flagcdn.com/w40/es.png",
+
+    video: "https://www.youtube.com/embed/_sfZ4L8pGzQ?si=dZkidMl0_LXVWbom",
+
+    singer: "Melody",
+    song: "ESA DIVA",
+    lyrics: "Переклад пісні тут...",
+  },
+  Україна: {
+    flag: "https://flagcdn.com/w40/ua.png",
+
+    video: "https://www.youtube.com/embed/0Jc02OFU42w?si=4WX8wU_HP9UyEvEd",
+    singer: "Ziferblat",
+    song: "Bird of Pray",
+    lyrics: "Переклад пісні тут...",
+  },
+  Швеція: {
+    flag: "https://flagcdn.com/w40/se.png",
+
+    video: "https://www.youtube.com/embed/WSh7U3m9KgA?si=J6Tlxle-BOtca8Y0",
+    singer: "KAJ",
+    song: "Bara Bada Bastu",
+    lyrics: "Переклад пісні тут...",
+  },
+  Португалія: {
+    flag: "https://flagcdn.com/w40/pt.png",
+
+    video: "https://www.youtube.com/embed/6QRHcLJS0T0?si=tghNqJxpOtFhXJYf",
+
+    singer: "NAPA",
+    song: "Deslocado",
+    lyrics: "Переклад пісні тут...",
+  },
+  Норвегія: {
+    flag: "https://flagcdn.com/w40/sj.png",
+
+    video: "https://www.youtube.com/embed/WReEi3jLRFQ?si=vJBqaPts05CKfo_m",
+
+    singer: "Kyle Alessandro",
+    song: "Lighter",
+    lyrics: "Переклад пісні тут...",
+  },
+  Бельгія: {
+    flag: "https://flagcdn.com/w40/be.png",
+
+    video: "https://www.youtube.com/embed/fl4LaADiLBY?si=D-m8ywX3s5pS59lg",
+
+    singer: "Red Sebastian",
+    song: "Strobe Lights",
+    lyrics: "Переклад пісні тут...",
+  },
+  Італія: {
+    flag: "https://flagcdn.com/w40/it.png",
+
+    video: "https://www.youtube.com/embed/0uT_Y3i7B6A?si=4oPxNQVqHTU5Z1_t",
+
+    singer: "Lucio Corsi",
+    song: "Volevo Essere Un Duro",
+    lyrics: "Переклад пісні тут...",
+  },
+  Азербайджан: {
+    flag: "https://flagcdn.com/w40/az.png",
+
+    video: "https://www.youtube.com/embed/wk1CUjaRKyo?si=rlBXT6gjv6pVegiH",
+
+    singer: "Mamagama",
+    song: "Run With U",
+    lyrics: "Переклад пісні тут...",
+  },
+  "Сан-Марино": {
+    flag: "https://flagcdn.com/w40/sm.png",
+
+    video: "https://www.youtube.com/embed/-vkDhFhgNX0?si=fcQuQ99khJwvASHX",
+
+    singer: "Gabry Ponte",
+    song: "Tutta L'Italia",
+    lyrics: "Переклад пісні тут...",
+  },
+  Албанія: {
+    flag: "https://flagcdn.com/w40/al.png",
+
+    video: "https://www.youtube.com/embed/cwVL1dbjqj4?si=X9VS90mpNeIUHtUz",
+
+    singer: "Shkodra Elektronike",
+    song: "Zjerm",
+    lyrics: "Переклад пісні тут...",
+  },
+  Нідерланди: {
+    flag: "https://flagcdn.com/w40/nl.png",
+
+    video: "https://www.youtube.com/embed/Wb8-gbgan80?si=KcbAvs4fb_699IXC",
+
+    singer: "Claude",
+    song: "C'est La Vie",
+    lyrics: "Переклад пісні тут...",
+  },
+  Хорватія: {
+    flag: "https://flagcdn.com/w40/hr.png",
+
+    video: "https://www.youtube.com/embed/jzK4D_gfRjQ?si=tSp9si6Zem0BuDqX",
+
+    singer: "Marko Bošnjak",
+    song: "Poison Cake",
+    lyrics: "Переклад пісні тут...",
+  },
+  Швейцарія: {
+    flag: "https://flagcdn.com/w40/ch.png",
+
+    video: "https://www.youtube.com/embed/F4lALgxBiYI?si=zTNlfvsNni2B5E0D",
+
+    singer: "Zoë Më",
+    song: "Voyage",
+    lyrics: "Переклад пісні тут...",
+  },
+  Кіпр: {
+    flag: "https://flagcdn.com/w40/cy.png",
+
+    video: "https://www.youtube.com/embed/egPAiAuC57k?si=MXoiaPzFPulRC258",
+
+    singer: "Theo Evan",
+    song: "Shh",
+    lyrics: "Переклад пісні тут...",
+  },
+  Австралія: {
+    flag: "https://flagcdn.com/w40/au.png",
+
+    video: "https://www.youtube.com/embed/EJ0RdIU_G8g?si=C2XO2DScDPle6oZ1",
+    singer: "Go-Jo",
+    song: "Milkshake Man ",
+    lyrics: "Переклад пісні тут...",
+  },
+  Чорногорія: {
+    flag: "https://flagcdn.com/w40/me.png",
+
+    video: "https://www.youtube.com/embed/QxpOxioeZJw?si=xu1oRkU_oG6FVW-z",
+    singer: "Nina Žižić",
+    song: "Dobrodošli",
+    lyrics: "Переклад пісні тут...",
+  },
+  Ірландія: {
+    flag: "https://flagcdn.com/w40/ie.png",
+
+    video: "https://www.youtube.com/embed/3MB628Kanzo?si=lnI-QGCHbLzAcZ92",
+
+    singer: "EMMY",
+    song: "Laika Party",
+    lyrics: "Переклад пісні тут...",
+  },
+  Латвія: {
+    flag: "https://flagcdn.com/w40/lv.png",
+
+    video: "https://www.youtube.com/embed/tRTfxUJtDOs?si=TARQS2uHnRVqxlVo",
+    singer: "Tautumeitas",
+    song: "Bur Man Laimi",
+    lyrics: "Переклад пісні тут...",
+  },
+  Вірменія: {
+    flag: "https://flagcdn.com/w40/am.png",
+
+    video: "https://www.youtube.com/embed/5sXbxyQ5K0M?si=iXhG2gvjF71xdxJC",
+    singer: "PARG",
+    song: "SURVIVOR",
+    lyrics: "Переклад пісні тут...",
+  },
+  Австрія: {
+    flag: "https://flagcdn.com/w40/at.png",
+
+    video: "https://www.youtube.com/embed/onOex2WXjbA?si=4u0hseCAt4es43es",
+    singer: "JJ",
+    song: "Wasted Love",
+    lyrics: "Переклад пісні тут...",
+  },
+  "Велика Британія": {
+    flag: "https://flagcdn.com/w40/gb.png",
+
+    video: "https://www.youtube.com/embed/wlZSQZm8j8c?si=bG9zFiV3r1R0InA4",
+
+    singer: "Remember Monday",
+    song: "What The Hell Just Happened?",
+    lyrics: "Переклад пісні тут...",
+  },
+  Греція: {
+    flag: "https://flagcdn.com/w40/gr.png",
+
+    video: "https://www.youtube.com/embed/OP9n_cO-6V4?si=5xZa3yYZh9BPBeKl",
+
+    singer: "Klavdia",
+    song: "Asteromáta",
+    lyrics: "Переклад пісні тут...",
+  },
+  Литва: {
+    flag: "https://flagcdn.com/w40/lt.png",
+
+    video: "https://www.youtube.com/embed/2vLHs89Akzo?si=-d7B5XW80hBvh2Av",
+
+    singer: "Katarsis",
+    song: "Tavo Akys",
+    lyrics: "Переклад пісні тут...",
+  },
+  Мальта: {
+    flag: "https://flagcdn.com/w40/mt.png",
+
+    video: "https://www.youtube.com/embed/2AN3vvLORuw?si=0bjDl7ZptsunK4nq",
+
+    singer: "Miriana Conte",
+    song: "SERVING",
+    lyrics: "Переклад пісні тут...",
+  },
+  Грузія: {
+    flag: "https://flagcdn.com/w40/ge.png",
+
+    video: "https://www.youtube.com/embed/jphJoo-CNtU?si=L2EWf6W9NRxTE_x_",
+
+    singer: "Mariam Shengelia",
+    song: "Freedom",
+    lyrics: "Переклад пісні тут...",
+  },
+  Франція: {
+    flag: "https://flagcdn.com/w40/fr.png",
+
+    video: "https://www.youtube.com/embed/TUyvT65c9ok?si=NY2kVCclmXXH1wkO",
+    singer: "Louane",
+    song: "maman",
+    lyrics: "Переклад пісні тут...",
+  },
+  Данія: {
+    flag: "https://flagcdn.com/w40/dk.png",
+
+    video: "https://www.youtube.com/embed/Q_ND8mF9d3c?si=GTx2BjeW3IYZziu6",
+
+    singer: "Sissal",
+    song: "Hallucination",
+    lyrics: "Переклад пісні тут...",
+  },
+  Чехія: {
+    flag: "https://flagcdn.com/w40/cz.png",
+
+    video: "https://www.youtube.com/embed/hdxna1DC7yo?si=dSK6tExcFbiyMzVv",
+    singer: "ADONXS",
+    song: "Kiss Kiss Goodbye",
+    lyrics: "Переклад пісні тут...",
+  },
+  Люксембург: {
+    flag: "https://flagcdn.com/w40/lu.png",
+
+    video: "https://www.youtube.com/embed/jMY-eUDTs0Q?si=mkJPeBXocCDoANrJ",
+
+    singer: "Laura Thorn",
+    song: "La Poupée Monte Le Son",
+    lyrics: "Переклад пісні тут...",
+  },
+  Ізраїль: {
+    flag: "https://flagcdn.com/w40/il.png",
+
+    video: "https://www.youtube.com/embed/R1J7nJXLKAg?si=9sP8-AUcNY6sXT7T",
+
+    singer: "Yuval Raphael",
+    song: "New Day Will Rise",
+    lyrics: "Переклад пісні тут...",
+  },
+  Німеччина: {
+    flag: "https://flagcdn.com/w40/de.png",
+
+    video: "https://www.youtube.com/embed/xF_sNQXP5g8?si=EOOxpuMWVGOWN1pz",
+
+    singer: "Abor & Tynna",
+    song: "Baller",
+    lyrics: "Переклад пісні тут...",
+  },
+  Сербія: {
+    flag: "https://flagcdn.com/w40/rs.png",
+
+    video: "https://www.youtube.com/embed/WlCoZ0UOXoY?si=EebYQyKlKlyOr2Y9",
+
+    singer: "Princ",
+    song: "Mila",
+    lyrics: "Переклад пісні тут...",
+  },
+  Фінляндія: {
+    flag: "https://flagcdn.com/w40/fi.png",
+
+    video: "https://www.youtube.com/embed/4m1c46KAc3o?si=E2pkdAgnIQrA7YO7",
+
+    singer: "Erika Vikman",
+    song: "ICH KOMME",
+    lyrics: "Переклад пісні тут...",
+  },
+};
+
+// Відображення випадаючого списку
+function showDropdown() {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.style.display = "block";
+  updateDropdown("");
+}
+
+// Оновлення списку при введенні
+function filterCountries() {
+  const searchValue = document
+    .getElementById("country-search")
+    .value.toLowerCase();
+  updateDropdown(searchValue);
+}
+
+// Заповнення випадаючого списку
+function updateDropdown(searchValue) {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.innerHTML = "";
+
+  Object.keys(countryData).forEach((country) => {
+    if (country.toLowerCase().includes(searchValue)) {
+      const div = document.createElement("div");
+      const img = document.createElement("img");
+      img.src = countryData[country].flag;
+      div.appendChild(img);
+
+      const text = document.createTextNode(country);
+      div.appendChild(text);
+
+      div.onclick = () => {
+        document.getElementById("country-search").value = country;
+        dropdown.style.display = "none";
+        changeContent(country);
+      };
+      dropdown.appendChild(div);
+    }
+  });
+}
+
+// Змінення контенту при виборі країни
+function changeContent(country) {
+  if (!countryData[country]) return;
+  document.getElementById("video-frame").src = countryData[country].video;
+  document.getElementById("singer-name").textContent =
+    "Виконавець: " + countryData[country].singer;
+  document.getElementById("song-info").textContent =
+    "Пісня: " + countryData[country].song;
+  document.getElementById("lyrics").textContent =
+    "Переклад: " + countryData[country].lyrics;
+}
